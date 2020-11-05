740c3d9604361d87982a80b3e0c16181faabdc1f
UsamaALiSultan <72700210+UsamaALiSultan@users.noreply.github.com>
2020-11-05 14:14:53 -0800

    first commit

---
+++ javaScripttask3.js
@@ -0,0 +1,209 @@
+// Question #1
+
+
+// var empty=[]
+// console.log(empty)
+
+// Question #3
+
+// var arry=['Car','House','Park','Jet']
+// console.log(arry)
+
+
+// Question #4
+
+// var arry=[1,2,3,4,5,6,7,8,9]
+// console.log(arry)
+
+// Question #5
+
+// var arry=[true,false] 
+// console.log(arry)
+
+// Question #6
+
+// var arry=['Car',true, 1 ,'Jet',false,5]
+// console.log(arry)
+
+
+
+// Question #7
+
+// var arry=['Zong','Ufone','jazz','telenor','Scom']
+// console.log(arry)
+
+
+// Question #8
+
+// var arry=['SSC', 'HSC', 'BCS', 'BS', 'BCOM', 'MS',
+// 'M.Phil', 'PhD']
+// console.log(arry)
+
+
+// Question #9
+// var movie=[]
+
+// movie.push('Avengers: Age of Ultron')
+
+// movie.push('Spectre')
+    
+// movie.push('Jrassic world')
+
+// movie.push('Inside out')
+// console.log(movie)
+
+
+
+// Question #10
+
+// var arry=['lamborghini','ferrari','audi','bugatti']
+// var lam=arry.indexOf('lamborghini')
+
+
+// console.log("First index is "+lam)
+ 
+// console.log("At first index the car is "+arry[0])
+
+// var bug=arry.indexOf("bugatti")
+
+// console.log("Last index is "+bug)
+
+// console.log("At last index the car is "+arry[3])
+
+
+// Question #11
+
+// var students=['john','tony','brian']
+// console.log(students)
+
+// var score=[320,230,480]
+// console.log("Score of "+ students[0]+
+// " is "+score[0]+"."+"Percentage: 64%")
+
+// console.log("Score of "+ students[1]+
+// " is "+score[1]+"."+"Percentage: 46%")
+
+// console.log("Score of "+ students[2]+
+// " is "+score[2]+"."+"Percentage: 96%")
+
+// Question #12
+
+// var colors=[]
+// console.log(colors)
+// var color1=prompt("pick a color to add")
+// console.log(
+//     colors.unshift (color1)
+// )
+
+// var color2=prompt("pick a color to add at the end")
+// console.log(
+//     colors.push (color2)
+// )
+
+// colors.unshift('green','blue')
+// colors.shift()
+
+// colors.pop()
+
+// console.log(colors)
+
+// var colors=["red","green","blue","yellow","black"]
+
+// var add=+prompt("enter the number from 0 to 4 to add the colors from "  )  
+
+// var add2=+prompt("how many colors you wish to add")
+
+// var add3=prompt("enter the color to add")
+
+// var add4=prompt("enter the 2nd color")
+
+// colors.splice(add,add2,add3,add4)
+
+// console.log(colors)
+
+// var remove=+prompt("enter the number from 0 to 4 to remove the colors"  )  
+
+// var remove2=+prompt("how many colors you wish to remove")
+
+
+// colors.splice(remove,remove2)
+
+
+// Question #13
+
+// var score=[230,120,300,400,500]
+// score.sort()
+// console.log(score)
+
+// Question #14
+
+// var fruits=['strawberry','apple' ,'orange' ,'banana']
+
+// fruits.sort()
+// console.log(fruits)
+
+// Question #15
+// var cities=["Karachi","Lahore","Islamabad","Quetta","Peshawar"]
+
+// var selected=cities.slice(2,4)
+// console.log(cities)
+// console.log(selected)
+
+//  Question #16
+// var arr = ["This ", "is " ,"my " ,"cat"]
+// var stringTo=arr.toString()
+// console.log(stringTo)
+
+// Question #17
+
+// var arr = []
+// arr.push("Keyboard")
+// arr.push("mouse")
+// arr.push("Printer")
+// arr.push("monitor")
+// console.log(arr.shift())
+// console.log(arr.shift())
+// console.log(arr.shift())
+// console.log(arr.shift())
+
+
+// Question #18
+
+//  var arr = []
+// arr.push("Keyboard")
+// arr.push("mouse")
+// arr.push("Printer")
+// arr.push("monitor")
+// arr.reverse()
+// console.log(arr.shift())
+// console.log(arr.shift())
+// console.log(arr.shift())
+// console.log(arr.shift())
+
+// Question #19
+
+// var repair=["Apple","Samsung","Motorola","Nokia","Sony","Haier"]
+// document.write(repair)
+// console.log(repair)
+
+
+//  Question #20
+
+// var multidimensional=[
+// Array(),
+// Array(),
+// Array()]
+
+// console.log(multidimensional)
+
+
+
+// #21
+
+// var multidimensional=[
+// Array(0,1,2,3),
+// Array(1,0,1,2),
+// Array(2,1,0,1)
+// ]
+
+// console.log(multidimensional)
